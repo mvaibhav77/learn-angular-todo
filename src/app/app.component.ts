b3cedc746e67f189708d28af319c43a88a9c5882
Vaibhav Shukla <shukla.vaibhav1077@gmail.com>
2024-05-24 10:48:24 +0530

    feat: added a simple about page to understand routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,17 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { HeaderComponent } from './components/header/header.component';
 import { TasksComponent } from './components/tasks/tasks.component';
+import { FooterComponent } from './components/footer/footer.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FontAwesomeModule, HeaderComponent, TasksComponent],
+  imports: [
+    RouterOutlet,
+    FontAwesomeModule,
+    HeaderComponent,
+    TasksComponent,
+    FooterComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
